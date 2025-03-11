81a813c75ceadb10bd07a4f40b060e18ab7a9a3c
郑博天 <2248384953@qq.com>
2025-03-11 08:14:35 +0800

    部署

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,24 +1,18 @@
-// Start from esbuild@^0.17.1
-// Multiple entryPoints in an array of object & pino transports
-import {build} from "esbuild";
-import esbuildPluginPino from "esbuild-plugin-pino";
-import esbuildPluginTsc from "esbuild-plugin-tsc";
+import { build } from 'esbuild';
+import esbuildPluginPino from 'esbuild-plugin-pino';
+import esbuildPluginTsc from 'esbuild-plugin-tsc';
 
-
-// --bundle --outfile=./dist/index.js --platform=node --target=node20
 build({
-    entryPoints: [
-        "./src/index.ts",
-    ],
-    outdir: "dist",
+    entryPoints: ['./src/index.ts'],
+    outdir: 'dist',
     bundle: true,
-    platform: "node",
-    target: "node20",
-    packages: 'external',
+    platform: 'node',
+    target: 'node20',
+    packages: 'external', // 这意味着不会将 node_modules 打包进去
     plugins: [
-        esbuildPluginPino({transports: ["pino-pretty"]}),
+        esbuildPluginPino({ transports: ['pino-pretty'] }),
         esbuildPluginTsc({
-            force: true
+            force: true,
         }),
     ],
 }).catch(() => process.exit(1));
