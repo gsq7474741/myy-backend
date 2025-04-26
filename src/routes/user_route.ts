d0fa6d20b00dc3a5c14b15bc700de8c5362d212b
Matsumoto Ruko <gsq7474741@icloud.com>
2025-04-26 16:46:30 +0800

    add health api (#26)
    * cancel data fluct
    
    * add health

--- src/routes/user_route.ts
+++ src/routes/user_route.ts
@@ -16,6 +16,7 @@ userRoutes.post('/login', userController.login.bind(userController));
 userRoutes.get('/users', authMiddleware, userController.getUsers);
 userRoutes.get('/users/id', authMiddleware, userController.getUserById);
 userRoutes.get('/users/get-mydev', authMiddleware, userController.getMyDev);
+userRoutes.get('/users/get-mydev-health', authMiddleware,userController.getMyDevHealth);
 userRoutes.get('/tree/task/:id/list', authMiddleware, userController.getTaskList);
 userRoutes.post('/users/upload-consultation', authMiddleware, userController.upload_consultations);
 userRoutes.post('/users/upload-work-order', authMiddleware, userController.upload_work_order);
